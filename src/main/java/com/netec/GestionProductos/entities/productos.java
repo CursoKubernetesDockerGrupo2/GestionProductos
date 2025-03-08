0d6d375e9ca4039b3886064d383a4750a9f39a86
Henry Saenz Lama <hsaenz21@gmail.com>
2025-03-08 18:40:34 -0500

    Cambios iniciales

---
+++ src/main/java/com/netec/GestionProductos/entities/productos.java
@@ -0,0 +1,54 @@
+package com.netec.GestionProductos.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name = "productos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class productos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
