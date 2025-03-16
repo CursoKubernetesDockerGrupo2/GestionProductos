bbd499a3452263ed5f3be23dd21522399b9ee470
Henry Saenz Lama <hsaenz21@gmail.com>
2025-03-16 15:37:17 -0500

    Cambios realizados

--- src/main/java/com/netec/GestionProductos/GestionProductosApplication.java
+++ src/main/java/com/netec/GestionProductos/GestionProductosApplication.java
@@ -3,10 +3,16 @@ package com.netec.GestionProductos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class GestionProductosApplication {
 
+	private static final Logger logger = Logger.getLogger(GestionProductosApplication.class.getName());
+
 	public static void main(String[] args) {
+		logger.log(Level.INFO, "Bienvenidos a GestionProductos - Grupo 2");
 		SpringApplication.run(GestionProductosApplication.class, args);
 	}
 
